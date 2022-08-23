32679f1ce00ce5de325415ab213f6051bcb34e1f
moha <moha729g@stud.kea.dk>
2022-08-23 09:25:29 +0200

    Initial commit

---
+++ src/main/java/com/mo/teknologi1/Teknologi1Application.java
@@ -0,0 +1,13 @@
+package com.mo.teknologi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teknologi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Teknologi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mo/teknologi1/Teknologi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mo.teknologi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teknologi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
